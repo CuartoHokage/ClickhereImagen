b97e5da73a98082559f12cbbe0976d1dedb24798
CuartoHokage <jaime199505@hotmail.com>
2020-05-07 11:06:28 -0500

    funcionando en el servidor

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const sql = require('mssql')
 //Configuracion de la conexión
 const config2 = {
     user: 'sa',
-    database: 'click_new',
-    password: '12qwaszx',
-    server: '192.168.1.113', // You can use 'localhost\\instance' to connect to named instance
-    // database: 'click',
-    // password: 'Clickhere2018',
-    // server: '192.168.2.102', // You can use 'localhost\\instance' to connect to named instance
+    // database: 'click_new',
+    // password: '12qwaszx',
+    // server: '192.168.1.113', // You can use 'localhost\\instance' to connect to named instance
+    database: 'click',
+    password: 'Clickhere2018',
+    server: '192.168.2.102', // You can use 'localhost\\instance' to connect to named instance
     pool: {
       max: 1000,
       min: 0,
